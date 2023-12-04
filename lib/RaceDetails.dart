1b1b7b98708315e9599f92607d09c61a932fa820
aschafer <01169233@pw.edu.pl>
2023-12-04 10:57:03 +0100

    Adjust race list and details to work with api

--- lib/RaceDetails.dart
+++ lib/RaceDetails.dart
@@ -1,22 +1,110 @@
+// import 'package:flutter/material.dart';
+//
+// class RaceDetails extends StatefulWidget {
+//   final int id;
+//
+//   RaceDetails(this.id);
+//
+//   @override
+//   _RaceDetailsState createState() => _RaceDetailsState();
+// }
+//
+// class _RaceDetailsState extends State<RaceDetails> {
+//   String selectedValue = 'Szosa'; // Set an initial selected value
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Race'),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(16.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Image.asset(
+//               'lib/sample_image.png',
+//               fit: BoxFit.cover,
+//             ),
+//             SizedBox(height: 16.0),
+//             Container(
+//               decoration: BoxDecoration(
+//                 border: Border.all(color: Colors.grey),
+//                 borderRadius: BorderRadius.circular(8.0),
+//               ),
+//               padding: EdgeInsets.all(12.0),
+//               child: Text(
+//                 'Additional Details:\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique lorem et volutpat fermentum. Praesent dapibus velit id auctor dignissim. Donec pharetra odio vel semper aliquam. Proin nec ligula in sem gravida fermentum vel in arcu. Nullam ac sem vel ante vehicula interdum. Curabitur ultrices rhoncus finibus. Phasellus eu libero. ',
+//                 style: TextStyle(fontSize: 16.0),
+//               ),
+//             ),
+//             SizedBox(height: 16.0),
+//             ElevatedButton(
+//               onPressed: () {
+//                 showAddTextDialog(context);
+//               },
+//               child: Row(
+//                 mainAxisSize: MainAxisSize.min,
+//                 children: [
+//                   Icon(Icons.add),
+//                   SizedBox(width: 8.0),
+//                   Text('Weź udział w wyścigu!'),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class RaceDetails extends StatefulWidget {
-  final String itemName;
+  final int id;
 
-  RaceDetails(this.itemName);
+  RaceDetails(this.id);
 
   @override
   _RaceDetailsState createState() => _RaceDetailsState();
 }
 
 class _RaceDetailsState extends State<RaceDetails> {
-  String selectedValue = 'Szosa'; // Set an initial selected value
+  String selectedValue = 'Szosa';
+  String raceName = '';
+  String raceDescription = '';
+
+  @override
+  void initState() {
+    super.initState();
+    // Fetch additional details for the race when the widget is created
+    fetchRaceDetails();
+  }
+
+  Future<void> fetchRaceDetails() async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:8000/api/rider/race/${widget.id}'));
+
+    if (response.statusCode == 200) {
+      // If the server returns a 200 OK response, parse the race details
+      final Map<String, dynamic> raceDetails = json.decode(utf8.decode(response.bodyBytes));
+      setState(() {
+        raceName = raceDetails['name'];
+        raceDescription = raceDetails['description'];
+      });
+    } else {
+      // If the server did not return a 200 OK response, throw an exception.
+      throw Exception('Failed to load race details');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.itemName),
+        title: Text(raceName),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
@@ -34,9 +122,13 @@ class _RaceDetailsState extends State<RaceDetails> {
                 borderRadius: BorderRadius.circular(8.0),
               ),
               padding: EdgeInsets.all(12.0),
-              child: Text(
-                'Additional Details:\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique lorem et volutpat fermentum. Praesent dapibus velit id auctor dignissim. Donec pharetra odio vel semper aliquam. Proin nec ligula in sem gravida fermentum vel in arcu. Nullam ac sem vel ante vehicula interdum. Curabitur ultrices rhoncus finibus. Phasellus eu libero. ',
-                style: TextStyle(fontSize: 16.0),
+              child: Column(
+                children: [
+                  Text(
+                    'Race Description: $raceDescription',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ],
               ),
             ),
             SizedBox(height: 16.0),
--- lib/RaceList.dart
+++ lib/RaceList.dart
@@ -1,8 +1,106 @@
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'RaceDetails.dart';
+//
+// class RaceList extends StatefulWidget {
+//   @override
+//   _RaceListState createState() => _RaceListState();
+// }
+//
+// class _RaceListState extends State<RaceList> {
+//   List<String> itemList = [];
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     // Fetch the list of races when the widget is created
+//     fetchRaceList();
+//   }
+//
+//   Future<void> fetchRaceList() async {
+//     final response = await http.get(Uri.parse('http://10.0.2.2:8000/api/rider/race/'));
+//
+//     if (response.statusCode == 200) {
+//       // If the server returns a 200 OK response, parse the races from the response
+//       final List<dynamic> races = json.decode(utf8.decode(response.bodyBytes));
+//       setState(() {
+//         itemList = races.map((race) => race['name'].toString()).toList();
+//       });
+//     } else {
+//       // If the server did not return a 200 OK response, throw an exception.
+//       throw Exception('Failed to load races');
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Scrollbar(
+//         child: ListView.builder(
+//           itemCount: itemList.length,
+//           itemBuilder: (context, index) {
+//             return ListTile(
+//               contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+//               leading: Container(
+//                 width: 60.0,
+//                 height: 60.0,
+//                 child: Image.asset(
+//                   'lib/sample_image.png',
+//                   fit: BoxFit.cover,
+//                 ),
+//               ),
+//               title: Text(itemList[index]),
+//               onTap: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                     builder: (context) => RaceDetails(itemList[index]),
+//                   ),
+//                 );
+//               },
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'RaceDetails.dart';
 
-class RaceList extends StatelessWidget {
-  final List<String> itemList = List.generate(15, (index) => 'Wyścig #$index');
+class RaceList extends StatefulWidget {
+  @override
+  _RaceListState createState() => _RaceListState();
+}
+
+class _RaceListState extends State<RaceList> {
+  List<Race> itemList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Fetch the list of races when the widget is created
+    fetchRaceList();
+  }
+
+  Future<void> fetchRaceList() async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:8000/api/rider/race'));
+
+    if (response.statusCode == 200) {
+      // If the server returns a 200 OK response, parse the races from the response
+      final List<dynamic> races = json.decode(utf8.decode(response.bodyBytes));
+      setState(() {
+        itemList = races.map((race) => Race.fromJson(race)).toList();
+      });
+    } else {
+      // If the server did not return a 200 OK response, throw an exception.
+      throw Exception('Failed to load races');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +112,19 @@ class RaceList extends StatelessWidget {
             return ListTile(
               contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
               leading: Container(
-                width: 60.0, // Set the width as needed
-                height: 60.0, // Set the height as needed
+                width: 60.0,
+                height: 60.0,
                 child: Image.asset(
-                  'lib/sample_image.png', // Replace with your image path
+                  'lib/sample_image.png',
                   fit: BoxFit.cover,
                 ),
               ),
-              title: Text(itemList[index]),
+              title: Text(itemList[index].name),
               onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => RaceDetails(itemList[index]),
+                    builder: (context) => RaceDetails(itemList[index].id),
                   ),
                 );
               },
@@ -34,6 +132,20 @@ class RaceList extends StatelessWidget {
           },
         ),
       ),
+    );
+  }
+}
+
+class Race {
+  final int id;
+  final String name;
+
+  Race({required this.id, required this.name});
+
+  factory Race.fromJson(Map<String, dynamic> json) {
+    return Race(
+      id: json['id'],
+      name: json['name'],
     );
   }
 }
