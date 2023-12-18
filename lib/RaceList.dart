9be0ea11a21869c7d5eacc8c63575453bf76a1a3
aschafer <01169233@pw.edu.pl>
2023-12-18 20:43:55 +0100

    Changed card appearance of ended races

--- lib/RaceList.dart
+++ lib/RaceList.dart
@@ -38,97 +38,6 @@ class _RaceListState extends State<RaceList> {
     }
   }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Dostępne wyścigi'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Scrollbar(
-//           child: ListView.builder(
-//             itemCount: itemList.length,
-//             itemBuilder: (context, index) {
-//               Color cardColor = Theme.of(context).colorScheme.surfaceVariant; // Default color
-//               // Check if the status is "ended" and set the card color to gray
-//               if (itemList[index].status == 'ended') {
-//                 cardColor = Theme.of(context).colorScheme.outlineVariant; // Set the color to gray
-//               }
-//               return Column(
-//                 children: [
-//                   GestureDetector(
-//                     onTap: () {
-//                       Navigator.push(
-//                         context,
-//                         MaterialPageRoute(
-//                           builder: (context) => RaceDetails(itemList[index].id),
-//                         ),
-//                       );
-//                     },
-//                     child: Card(
-//                       margin: const EdgeInsets.all(5.0),
-//                       color: cardColor, // Set the color based on the status
-//                       child: Column(
-//                         children: [
-//                           Container(
-//                             height: 160.0,
-//                             width: double.infinity,
-//                             child: ClipRRect(
-//                               borderRadius: BorderRadius.only(
-//                                 topLeft: Radius.circular(16.0),
-//                                 topRight: Radius.circular(16.0),
-//                               ),
-//                               child: Image.asset(
-//                                 'lib/sample_image.png',
-//                                 fit: BoxFit.fitWidth,
-//                               ),
-//                             ),
-//                           ),
-//                           ListTile(
-//                             contentPadding: const EdgeInsets.all(10.0),
-//                             title: Column(
-//                               crossAxisAlignment: CrossAxisAlignment.start,
-//                               children: [
-//                                 Text(
-//                                   itemList[index].name,
-//                                   style: Theme.of(context).textTheme.titleLarge,
-//                                 ),
-//                                 SizedBox(height: 5.0),
-//                                 Text(
-//                                   '${formatDateString(itemList[index].timeStart)}-${formatDateStringToHours(itemList[index].timeEnd)}',
-//                                   style: Theme.of(context).textTheme.bodyMedium,
-//                                 ),
-//                               ],
-//                             ),
-//                           ),
-//                         ],
-//                       ),
-//                     ),
-//                   ),
-//                   SizedBox(height: 8.0), // Add space between list elements
-//                 ],
-//               );
-//             },
-//           ),
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton.extended(
-//         onPressed: () {
-//           Navigator.push(
-//             context,
-//             MaterialPageRoute(
-//               builder: (context) => RaceParticipation(),
-//             ),
-//           );
-//         },
-//         label: Text('Rozpocznij wyścig'),
-//         icon: Icon(Icons.pedal_bike),
-//       ),
-//     );
-//   }
-// }
-
 @override
 Widget build(BuildContext context) {
   return Scaffold(
@@ -143,11 +52,6 @@ Widget build(BuildContext context) {
           child: ListView.builder(
             itemCount: itemList.length,
             itemBuilder: (context, index) {
-              Color cardColor = Theme.of(context).cardColor; // Default color
-              // Check if the status is "ended" and set the card color to gray
-              if (itemList[index].status == 'ended') {
-                cardColor = Theme.of(context).colorScheme.secondaryContainer; // Set the color to gray
-              }
               return Column(
                 children: [
                   GestureDetector(
@@ -159,43 +63,47 @@ Widget build(BuildContext context) {
                         ),
                       );
                     },
-                    child: Card(
-                      margin: const EdgeInsets.all(5.0),
-                      color: cardColor, // Set the color based on the status
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 160.0,
-                            width: double.infinity,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(16.0),
-                                topRight: Radius.circular(16.0),
-                              ),
-                              child: Image.asset(
-                                'lib/sample_image.png',
-                                fit: BoxFit.fitWidth,
+                    child: ColorFiltered(colorFilter: ColorFilter.mode(
+                        itemList[index].status == 'ended' ? Theme.of(context).colorScheme.surface.withOpacity(0.62) : Colors.transparent,
+                        BlendMode.srcOver,
+                    ),
+                      child: Card(
+                        margin: const EdgeInsets.all(5.0),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 160.0,
+                              width: double.infinity,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(16.0),
+                                  topRight: Radius.circular(16.0),
+                                ),
+                                child: Image.asset(
+                                  'lib/sample_image.png',
+                                  fit: BoxFit.fitWidth,
+                                ),
                               ),
                             ),
-                          ),
-                          ListTile(
-                            contentPadding: const EdgeInsets.all(10.0),
-                            title: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  itemList[index].name,
-                                  style: Theme.of(context).textTheme.titleLarge,
-                                ),
-                                SizedBox(height: 5.0),
-                                Text(
-                                  '${formatDateString(itemList[index].timeStart)}-${formatDateStringToHours(itemList[index].timeEnd)}',
-                                  style: Theme.of(context).textTheme.bodyMedium,
-                                ),
-                              ],
+                            ListTile(
+                              contentPadding: const EdgeInsets.all(10.0),
+                              title: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    itemList[index].name,
+                                    style: Theme.of(context).textTheme.titleLarge,
+                                  ),
+                                  SizedBox(height: 5.0),
+                                  Text(
+                                    '${formatDateString(itemList[index].timeStart)}-${formatDateStringToHours(itemList[index].timeEnd)}',
+                                    style: Theme.of(context).textTheme.bodyMedium,
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
