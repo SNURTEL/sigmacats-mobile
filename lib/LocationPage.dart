7d07a720f9ad4737516c72c34492567177e6664f
Tomasz Owienko <tomek.owienko@gmail.com>
2023-12-28 21:00:49 +0100

    Add basic map

--- lib/LocationPage.dart
+++ lib/LocationPage.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:latlong2/latlong.dart';
 import 'BottomNavigationBar.dart';
 import 'package:flutter_background_geolocation/flutter_background_geolocation.dart' as bg;
-
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 class LocationPage extends StatefulWidget {
   const LocationPage({Key? key}) : super(key: key);
@@ -16,6 +18,24 @@ class _LocationPageState extends State<LocationPage> {
   bool isTracking = false;
 
   bg.Location? location;
+
+  final mapController = MapController();
+
+  TileLayer get openStreetMapTileLayer => TileLayer(
+        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+        userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+        // Use the recommended flutter_map_cancellable_tile_provider package to
+        // support the cancellation of loading tiles.
+        tileProvider: CancellableNetworkTileProvider(),
+      );
+  
+  MarkerLayer get markerLayer => MarkerLayer(
+      markers: location != null ? [Marker(
+          point: LatLng(
+            location!.coords.latitude, location!.coords.longitude
+          ),
+          child: Icon(Icons.person))] : []
+  );
 
   @override
   void initState() {
@@ -54,8 +74,14 @@ class _LocationPageState extends State<LocationPage> {
         persistMode: bg.Config.PERSIST_MODE_LOCATION,
         startOnBoot: true,
         debug: true,
-        logLevel: bg.Config.LOG_LEVEL_VERBOSE
-    ));
+        logLevel: bg.Config.LOG_LEVEL_VERBOSE));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    mapController.dispose();
   }
 
   @override
@@ -71,6 +97,24 @@ class _LocationPageState extends State<LocationPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Expanded(
+              child: FlutterMap(
+                mapController: mapController,
+                options: MapOptions(
+                  initialCenter: LatLng(52.23202828872916, 21.006132649819673), // Warsaw
+                  initialZoom: 20,
+                  // interactionOptions:
+                  // InteractionOptions(flags: uploadedGpxObject != null ? InteractiveFlag.all : InteractiveFlag.none)
+                ),
+                children: [
+                  openStreetMapTileLayer,
+                  markerLayer
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
             Text("Toggle tracking"),
             Switch(
                 value: isTracking,
@@ -86,19 +130,24 @@ class _LocationPageState extends State<LocationPage> {
                   } else {
                     bg.BackgroundGeolocation.stop();
                   }
-                }
+                }),
+            SizedBox(
+              height: 16,
             ),
-            SizedBox(height: 32,),
             FilledButton(
                 onPressed: () {
                   setState(() {
                     bg.BackgroundGeolocation.getCurrentPosition(
-                        maximumAge: 0,
-                        persist: false,     // <-- do not persist this location
-                        desiredAccuracy: 0, // <-- desire best possible accuracy
-                        timeout: 30000,     // <-- wait 30s before giving up.
-                        samples: 1          // <-- sample 3 location before selecting best.
-                    ).then((bg.Location location) {
+                            maximumAge: 0,
+                            persist: false,
+                            // <-- do not persist this location
+                            desiredAccuracy: 0,
+                            // <-- desire best possible accuracy
+                            timeout: 30000,
+                            // <-- wait 30s before giving up.
+                            samples: 1 // <-- sample 3 location before selecting best.
+                            )
+                        .then((bg.Location location) {
                       print('[getCurrentPosition] - $location');
                       this.location = location;
                     }).catchError((error) {
@@ -107,14 +156,21 @@ class _LocationPageState extends State<LocationPage> {
                   });
                 },
                 child: Text("Update location")),
-            SizedBox(height: 16,),
-            Text("Last location:", style: Theme.of(context).textTheme.labelLarge,),
+            SizedBox(
+              height: 16,
+            ),
+            Text(
+              "Last location:",
+              style: Theme.of(context).textTheme.labelLarge,
+            ),
             Text("lat: ${location?.coords.latitude}\n"
                 "lon: ${location?.coords.longitude}\n"
                 "last update: ${location?.timestamp}\n"
                 "activity type: ${location?.activity.type}\n"
                 "confidence: ${location?.activity.confidence}"),
-            SizedBox(height: 16,),
+            SizedBox(
+              height: 16,
+            ),
             Text("Samples: ${num_samples}"),
           ],
         ),
@@ -128,19 +184,19 @@ class _LocationPageState extends State<LocationPage> {
           // Handle navigation based on index
           switch (currentIndex) {
             case 0:
-            // Wyścigi
+              // Wyścigi
               Navigator.pushReplacementNamed(context, '/race_list', arguments: "");
               break;
             case 1:
-            // Ranking
+              // Ranking
               Navigator.pushReplacementNamed(context, '/ranking', arguments: "");
               break;
             case 2:
-            // Aktualny wyścig
+              // Aktualny wyścig
               Navigator.pushReplacementNamed(context, '/race_participation', arguments: "");
               break;
             case 3:
-            // Mój profil
+              // Mój profil
               Navigator.pushReplacementNamed(context, '/user_profile', arguments: "");
               break;
             case 4:
