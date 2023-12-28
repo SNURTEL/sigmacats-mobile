8afeb2d090af6626c0b286aac14699012ed3e94c
aschafer <01169233@pw.edu.pl>
2023-12-28 21:28:46 +0100

    Fixed validation handling

--- lib/ForgotPasswordPage.dart
+++ lib/ForgotPasswordPage.dart
@@ -9,24 +9,26 @@ class ForgotPasswordPage extends StatefulWidget {
 }
 
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
-
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _emailController = TextEditingController();
 
   Future<void> _sendEmail(BuildContext context) async {
-    String email = _emailController.text;
-    final apiUrl = Uri.parse('http://10.0.2.2:8000/api/auth/forgot-password');
+    if (_formKey.currentState?.validate() ?? false) {
+      String email = _emailController.text;
+      final apiUrl = Uri.parse('http://10.0.2.2:8000/api/auth/forgot-password');
 
-    try {
-      final response = await http.post(apiUrl, body: {'email': email});
+      try {
+        final response = await http.post(apiUrl, body: {'email': email});
 
-      if (response.statusCode == 202) {
-        showNotification(context, "Wysłano wiadomość! Sprawdź skrzynkę.");
-      } else {
-        showNotification(context, 'Niepoprawny adres email.');
+        if (response.statusCode == 202) {
+          showNotification(context, "Wysłano wiadomość! Sprawdź skrzynkę.");
+        } else {
+          showNotification(context, 'Niepoprawny adres email.');
+        }
+      } catch (e) {
+        // Handle any exceptions or network errors
+        showNotification(context, 'Wystąpił błąd.');
       }
-    } catch (e) {
-      // Handle any exceptions or network errors
-      showNotification(context, 'Wystąpił błąd.');
     }
   }
 
@@ -38,51 +40,52 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Wprowadź adres email powiązany z kontem - wyślemy do Ciebie wiadomość z linkiem\ndo zresetowania hasła.",
-              style: Theme.of(context).textTheme.bodyLarge,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20.0),
-            SizedBox(
-              height: 70.0,
-              child: TextFormField(
-                validator: (v) {
-                  if (v == null || v.isEmpty) {
-                    return "Adres email nie może być pusty.";
-                  }
-                  if (!RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
-                      .hasMatch(v)) {
-                    return "Niepoprawny adres email.";
-                  }
-                  return null;
-                },
-                controller: _emailController,
-                decoration: const InputDecoration(
-                  labelText: 'Adres e-mail',
-                  enabledBorder: OutlineInputBorder(),
-                  focusedBorder: OutlineInputBorder(),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Wprowadź adres email powiązany z kontem - wyślemy do Ciebie wiadomość z linkiem\ndo zresetowania hasła.",
+                style: Theme.of(context).textTheme.bodyLarge,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30.0),
+              Flexible(
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Adres email nie może być pusty.";
+                    }
+                    if (!RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$')
+                        .hasMatch(value)) {
+                      return "Niepoprawny adres email.";
+                    }
+                    return null;
+                  },
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    labelText: 'Adres e-mail',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20.0),
-            FilledButton(
-              onPressed: () {
-                _sendEmail(context);
-              },
-              style: ElevatedButton.styleFrom(
-                fixedSize: const Size(double.infinity, 50.0),
+              const SizedBox(height: 30.0),
+              FilledButton(
+                onPressed: () {
+                  _sendEmail(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(double.infinity, 50.0),
+                ),
+                child: const Text(
+                  'Wyślij wiadomość',
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
               ),
-              child: const Text(
-                'Wyślij wiadomość',
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
