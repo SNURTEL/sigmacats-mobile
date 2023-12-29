aa07ce3245a393f35c7f23d61595e8db997404db
Miloszun <mizak.milosz@gmail.com>
2023-12-29 01:36:22 +0100

    fruitless attempts in getting a mock league panel to work

--- lib/Ranking.dart
+++ lib/Ranking.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'BottomNavigationBar.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Ranking extends StatefulWidget {
   final String accessToken;
@@ -11,6 +13,37 @@ _RankingState createState() => _RankingState();
 
 class _RankingState extends State<Ranking> {
   int currentIndex = 1;
+  int seasonID = 1; // TODO: add endpoint to get the current season
+  List<Classification> classificationsParsed = [
+    Classification(id: 1, name: 'test', description: 'test')
+  ];
+  String dropdownValue = 'test';
+
+  Future<void> fetchClassifications() async {
+    final response = await http.get(
+      Uri.parse('http://10.0.2.2:8000/api/rider/season/$seasonID/classification'),
+      headers: {'Authorization': 'Bearer ${widget.accessToken}'},
+    );
+
+    if (response.statusCode == 200) {
+      // If the server returns a 200 OK response, parse the classifications from the response
+      final List<dynamic> classifications = json.decode(utf8.decode(response.bodyBytes));
+      setState(() {
+        classificationsParsed = classifications.map((classification) => Classification.fromJson(classification)).toList();
+      });
+    } else {
+      // If the server did not return a 200 OK response, throw an exception
+      throw Exception('Failed to load classifications');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_){
+      //fetchClassifications();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +53,37 @@ class _RankingState extends State<Ranking> {
           automaticallyImplyLeading: false,
           centerTitle: true,
         ),
-        body: Center(
-          // Your widgets can go here
+        body: ListView(
+            children: <Widget> [
+              DropdownButton<String>(
+                value: dropdownValue,
+                icon: const Icon(Icons.arrow_downward),
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),
+                onChanged: (String? value) {
+                  // This is called when the user selects an item.
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+                items: classificationsParsed.map((c) => c.name).map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              ListView(
+                padding: const EdgeInsets.all(8),
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                children: <Widget>[],
+              ),
+            ]
         ),
       bottomNavigationBar: BottomNavigationBarWidget(
         currentIndex: currentIndex,
@@ -49,6 +111,22 @@ class _RankingState extends State<Ranking> {
           }
         },
       ),
+    );
+  }
+}
+
+class Classification {
+  final int id;
+  final String name;
+  final String description;
+
+  Classification({required this.id, required this.name, required this.description});
+
+  factory Classification.fromJson(Map<String, dynamic> json) {
+    return Classification(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
     );
   }
 }
