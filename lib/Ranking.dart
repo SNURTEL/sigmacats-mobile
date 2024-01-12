6af75bccd403d1eba5647ac45192c0f67850e4ed
Mewash1 <mizak.milosz@gmail.com>
2024-01-12 01:57:18 +0100

    the widget works... kinda XD

--- lib/Ranking.dart
+++ lib/Ranking.dart
@@ -15,9 +15,11 @@ class _RankingState extends State<Ranking> {
   int currentIndex = 1;
   int seasonID = 0;
   List<Classification> classificationsParsed = [];
-  String dropdownValue = 'test';
+  List<ScoreRow> scoreRows = [];
+  String dropdownValue = 'Wybierz klasyfikację';
 
   Future<void> fetchClassifications() async {
+    await fetchCurrentSeason();
     final response = await http.get(
       Uri.parse('http://10.0.2.2:8000/api/rider/season/$seasonID/classification'),
       headers: {'Authorization': 'Bearer ${widget.accessToken}'},
@@ -28,6 +30,7 @@ class _RankingState extends State<Ranking> {
       final List<dynamic> classifications = json.decode(utf8.decode(response.bodyBytes));
       setState(() {
         classificationsParsed = classifications.map((classification) => Classification.fromJson(classification)).toList();
+        classificationsParsed.insert(0, Classification(id: 0, name: 'Wybierz klasyfikację', description: 'brak'));
       });
     } else {
       // If the server did not return a 200 OK response, throw an exception
@@ -52,10 +55,54 @@ class _RankingState extends State<Ranking> {
     }
   }
 
+  Future<void> fetchScores(int classificationId) async {
+    List<RiderClassificationLink> scoresLocal = [];
+    List<Rider> ridersParsed = [];
+    List<ScoreRow> scoreRows = [];
+
+    if (classificationId != 0) {
+      final response = await http.get(
+        Uri.parse('http://10.0.2.2:8000/api/rider/rider_classification_link/$classificationId/classification'),
+        headers: {'Authorization': 'Bearer ${widget.accessToken}'},
+      );
+
+      if (response.statusCode == 200) {
+        // If the server returns a 200 OK response, parse the scores from the response
+        final List<dynamic> scores = json.decode(utf8.decode(response.bodyBytes));
+        scoresLocal = scores.map((score) => RiderClassificationLink.fromJson(score)).toList();
+      } else {
+        // If the server did not return a 200 OK response, throw an exception
+        throw Exception('Failed to load scores');
+      }
+
+      final responseRiders = await http.get(
+        Uri.parse('http://10.0.2.2:8000/api/rider/classification/$classificationId/rider'),
+        headers: {'Authorization': 'Bearer ${widget.accessToken}'},
+      );
+
+      if (responseRiders.statusCode == 200) {
+        final List<dynamic> riders = json.decode(utf8.decode(responseRiders.bodyBytes));
+        ridersParsed = riders.map((rider) => Rider.fromJson(rider)).toList();
+      } else {
+        throw Exception('Failed to load riders');
+      }
+
+      for (RiderClassificationLink score in scoresLocal) {
+        for (Rider rider in ridersParsed) {
+          if (score.rider_id == rider.id) {
+            scoreRows.add(ScoreRow(score: score.score, name: rider.name, surname: rider.surname));
+          }
+        }
+      }
+    }
+    setState(() {
+      this.scoreRows = scoreRows;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    fetchCurrentSeason();
     fetchClassifications();
   }
 
@@ -63,7 +110,7 @@ class _RankingState extends State<Ranking> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Ranking'),
+          title: const Text('Ranking'),
           automaticallyImplyLeading: false,
           centerTitle: true,
         ),
@@ -79,7 +126,13 @@ class _RankingState extends State<Ranking> {
                   color: Colors.deepPurpleAccent,
                 ),
                 onChanged: (String? value) {
-                  // This is called when the user selects an item.
+                  int classificationId = 0;
+                  for (final classification in classificationsParsed) {
+                    if (classification.name == value) {
+                      classificationId = classification.id;
+                    }
+                  }
+                  fetchScores(classificationId);
                   setState(() {
                     dropdownValue = value!;
                   });
@@ -91,12 +144,28 @@ class _RankingState extends State<Ranking> {
                   );
                 }).toList(),
               ),
-              ListView(
-                padding: const EdgeInsets.all(8),
-                shrinkWrap: true,
-                physics: const ClampingScrollPhysics(),
-                children: <Widget>[],
-              ),
+              ListView.builder(
+                itemCount: scoreRows.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        contentPadding: const EdgeInsets.all(10.0),
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${scoreRows[index].name} ${scoreRows[index].surname}",
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                            const SizedBox(height: 5.0),
+                          ],
+                        ),
+                      )
+                    ],
+                  );
+                },
+              )
             ]
         ),
       bottomNavigationBar: BottomNavigationBarWidget(
@@ -127,6 +196,8 @@ class _RankingState extends State<Ranking> {
       ),
     );
   }
+
+
 }
 
 class Classification {
@@ -148,8 +219,8 @@ class Classification {
 class Season {
   final int id;
   final String name;
-  final DateTime startTimestamp;
-  final DateTime endTimestamp;
+  final String startTimestamp;
+  final String endTimestamp;
 
   Season({required this.id, required this.name, required this.startTimestamp, required this.endTimestamp});
 
@@ -161,4 +232,44 @@ class Season {
       endTimestamp: json['end_timestamp'],
     );
   }
+}
+
+class RiderClassificationLink {
+  final int score;
+  final int rider_id;
+  final int classification_id;
+
+  RiderClassificationLink({required this.score, required this.rider_id, required this.classification_id});
+
+  factory RiderClassificationLink.fromJson(Map<String, dynamic> json) {
+    return RiderClassificationLink(
+      score: json['score'],
+      rider_id: json['rider_id'],
+      classification_id: json['classification_id']
+    );
+  }
+}
+
+class Rider {
+  final int id;
+  final String name;
+  final String surname;
+
+  Rider({required this.id, required this.name, required this.surname});
+
+  factory Rider.fromJson(Map<String, dynamic> json) {
+    return Rider(
+        id: json['id'],
+        name: json['account']['name'],
+        surname: json['account']['surname']
+    );
+  }
+}
+
+class ScoreRow {
+  final int score;
+  final String name;
+  final String surname;
+
+  ScoreRow({required this.score, required this.name, required this.surname});
 }
