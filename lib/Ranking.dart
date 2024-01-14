d53c044700afa3510c79ac2f51f4ebdd34af2caf
Miloszun <mizak.milosz@gmail.com>
2024-01-14 01:44:44 +0100

    ranking basically done

--- lib/Ranking.dart
+++ lib/Ranking.dart
@@ -11,15 +11,17 @@ class Ranking extends StatefulWidget {
   _RankingState createState() => _RankingState();
 }
 
-class _RankingState extends State<Ranking> {
+class _RankingState extends State<Ranking>{
   int currentIndex = 1;
   int seasonID = 0;
   List<Classification> classificationsParsed = [];
   List<ScoreRow> scoreRows = [];
-  String dropdownValue = 'Wybierz klasyfikację';
+  List<Season> seasonsParsed = [];
+  User currentUser = User(name: 'default', surname: 'default', username: 'd');
+  String classificationDropdownValue = 'Klasyfikacja generalna';
+  String seasonDropdownValue = 'Wybierz sezon';
 
   Future<void> fetchClassifications() async {
-    await fetchCurrentSeason();
     final response = await http.get(
       Uri.parse('http://10.0.2.2:8000/api/rider/season/$seasonID/classification'),
       headers: {'Authorization': 'Bearer ${widget.accessToken}'},
@@ -50,8 +52,40 @@ class _RankingState extends State<Ranking> {
         seasonID = Season.fromJson(season).id;
       });
     } else {
-      // If the server did not return a 200 OK response, throw an exception
       throw Exception('Failed to load current season');
+    }
+  }
+
+  Future<void> fetchSeasons() async {
+    final response = await http.get(
+      Uri.parse('http://10.0.2.2:8000/api/rider/season/all'),
+      headers: {'Authorization': 'Bearer ${widget.accessToken}'},
+    );
+
+    if (response.statusCode == 200) {
+      final List<dynamic> seasons = json.decode(utf8.decode(response.bodyBytes));
+      setState(() {
+        seasonsParsed = seasons.map((season) => Season.fromJson(season)).toList();
+        seasonsParsed.insert(0, Season(id: 0, name: 'Wybierz sezon', startTimestamp: 'brak', endTimestamp: 'brak'));
+      });
+    } else {
+      throw Exception('Failed to load seasons');
+    }
+  }
+  
+  Future<void> fetchCurrentUser() async {
+    final response = await http.get(
+      Uri.parse('http://10.0.2.2:8000/api/users/me'),
+      headers: {'Authorization': 'Bearer ${widget.accessToken}'},
+    );
+
+    if (response.statusCode == 200) {
+      final user = json.decode(utf8.decode(response.bodyBytes));
+      setState(() {
+        currentUser = User.fromJson(user);
+      });
+    } else {
+      throw Exception('Failed to load current user');
     }
   }
 
@@ -81,6 +115,17 @@ class _RankingState extends State<Ranking> {
     }
   }
 
+  Future<void> fetchInitialData() async {
+    await fetchCurrentSeason();
+    await fetchSeasons();
+    setState(() {
+      seasonDropdownValue = getSeasonName();
+    });
+    await fetchClassifications();
+    await fetchCurrentUser();
+    await fetchScores(getClassificationId('Klasyfikacja generalna'));
+  }
+
   Color getColor(int index) {
     if (index == 0) {
       return Colors.amber;
@@ -89,17 +134,44 @@ class _RankingState extends State<Ranking> {
       return Colors.blueGrey.shade300;
     }
     else if (index == 2) {
-      return Colors.deepOrange.shade800;
+      return Colors.deepOrange;
     }
     else {
       return Colors.white;
     }
   }
 
+  Color getCardColor(int index) {
+    if (scoreRows[index].name == currentUser.name && scoreRows[index].surname == currentUser.surname) {
+      return Colors.deepPurple.shade100;
+    }
+    else {
+      return Colors.white;
+    }
+  }
+  
+  int getClassificationId(String? name) {
+    for (final classification in classificationsParsed) {
+      if (classification.name == name) {
+        return classification.id;
+      }
+    }
+    return 0;
+  }
+
+  String getSeasonName() {
+    for (final season in seasonsParsed) {
+      if (season.id == seasonID) {
+        return season.name;
+      }
+    }
+    return 'Wybierz sezon';
+  }
+
   @override
   void initState() {
     super.initState();
-    fetchClassifications();
+    fetchInitialData();
   }
 
   @override
@@ -112,33 +184,70 @@ class _RankingState extends State<Ranking> {
         ),
         body: Column(
             children: <Widget> [
-              DropdownButton<String>(
-                value: dropdownValue,
-                icon: const Icon(Icons.arrow_downward),
-                elevation: 16,
-                style: const TextStyle(color: Colors.deepPurple),
-                underline: Container(
-                  height: 2,
-                  color: Colors.deepPurpleAccent,
-                ),
-                onChanged: (String? value) {
-                  int classificationId = 0;
-                  for (final classification in classificationsParsed) {
-                    if (classification.name == value) {
-                      classificationId = classification.id;
-                    }
-                  }
-                  fetchScores(classificationId);
-                  setState(() {
-                    dropdownValue = value!;
-                  });
-                },
-                items: classificationsParsed.map((c) => c.name).map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16),
+                    child: DropdownButton<String>(
+                      value: classificationDropdownValue,
+                      icon: const Icon(Icons.arrow_downward),
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.deepPurple),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      onChanged: (String? value) {
+                        int classificationId = getClassificationId(value);
+                        fetchScores(classificationId);
+                        setState(() {
+                          classificationDropdownValue = value!;
+                        });
+                      },
+                      items: classificationsParsed.map((c) => c.name).map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 80),
+                    child: DropdownButton<String>(
+                      value: seasonDropdownValue,
+                      icon: const Icon(Icons.arrow_downward),
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.deepPurple),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.deepPurpleAccent,
+                      ),
+                      onChanged: (String? value) {
+                        for (final season in seasonsParsed) {
+                          if (season.name == value) {
+                            setState(() {
+                              seasonID = season.id;
+                            });
+                            break;
+                          }
+                        }
+                        fetchClassifications();
+                        setState(() {
+                          seasonDropdownValue = value!;
+                          classificationDropdownValue = 'Klasyfikacja generalna';
+                          scoreRows = [];
+                        });
+                      },
+                      items: seasonsParsed.map((s) => s.name).map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                ],
               ),
               Expanded(
                 child: ListView.builder(
@@ -146,31 +255,67 @@ class _RankingState extends State<Ranking> {
                   itemBuilder: (context, index) {
                     return Column(
                       children: [
-                        SizedBox(
-                          width: double.infinity,
-                          child: Card(
-                            color: getColor(index),
-                            child: Padding(
-                              padding: const EdgeInsets.all(12.0),
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                    child: Text(
-                                      "${scoreRows[index].name} ${scoreRows[index].surname}",
-                                      style: Theme.of(context).textTheme.titleMedium,
-                                      textAlign: TextAlign.left,
-                                    ),
-                                  ),
-                                  Text(
-                                    "${scoreRows[index].score}",
-                                    style: Theme.of(context).textTheme.titleMedium,
-                                    textAlign: TextAlign.right,
-                                  ),
-                                ],
+                        Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                left: 16.0,
+                                bottom: 16.0,
+                                right: 16.0
+                              ),
+                              child: Text(
+                                "${index+1}",
+                                style: Theme.of(context).textTheme.titleLarge,
                               ),
                             ),
-                          ),
-                        ),
+                            Expanded(
+                                child: SizedBox(
+                                  width: double.infinity,
+                                  child: Card(
+                                    color: getCardColor(index),
+                                    margin: const EdgeInsets.only(
+                                        right: 16.0,
+                                        bottom: 16.0),
+                                    shape: RoundedRectangleBorder(
+                                        side: BorderSide(color: getColor(index), width: 3.0),
+                                        borderRadius: BorderRadius.circular(10.0)
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(12.0),
+                                      child: Row(
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.all(0.0),
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.start,
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  scoreRows[index].username,
+                                                  style: Theme.of(context).textTheme.titleLarge,
+                                                ),
+                                                Text(
+                                                  "${scoreRows[index].name} ${scoreRows[index].surname}",
+                                                  style: Theme.of(context).textTheme.labelMedium,
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          Expanded(
+                                            child: Text(
+                                              "${scoreRows[index].score}",
+                                              style: Theme.of(context).textTheme.titleMedium,
+                                              textAlign: TextAlign.right,
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                            )
+                          ],
+                        )
                       ],
                     );
                   },
@@ -206,8 +351,6 @@ class _RankingState extends State<Ranking> {
       ),
     );
   }
-
-
 }
 
 class Classification {
@@ -221,7 +364,7 @@ class Classification {
     return Classification(
       id: json['id'],
       name: json['name'],
-      description: json['description'],
+      description: json['description']
     );
   }
 }
@@ -230,7 +373,7 @@ class Season {
   final int id;
   final String name;
   final String startTimestamp;
-  final String endTimestamp;
+  final String? endTimestamp;
 
   Season({required this.id, required this.name, required this.startTimestamp, required this.endTimestamp});
 
@@ -248,14 +391,32 @@ class ScoreRow {
   final int score;
   final String name;
   final String surname;
+  final String username;
 
-  ScoreRow({required this.score, required this.name, required this.surname});
+  ScoreRow({required this.score, required this.name, required this.surname, required this.username});
 
   factory ScoreRow.fromJson(Map<String, dynamic> json) {
     return ScoreRow(
         score: json['score'],
         name: json['name'],
-        surname: json['surname']
+        surname: json['surname'],
+        username: json['username']
+    );
+  }
+}
+
+class User {
+  final String name;
+  final String surname;
+  final String username;
+  
+  User({required this.name, required this.surname, required this.username});
+  
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        name: json['name'],
+        surname: json['surname'],
+        username: json['username']
     );
   }
 }
